ba882f8f7566c7c0db8ba86559e181570fe6473c
Blopausore <augustin_650@hotmail.fr>
2023-07-30 15:19:58 +0200

    ADDED : Scoring speed, need to be tested

--- src/environment/score_function.py
+++ src/environment/score_function.py
@@ -1,3 +1,5 @@
+import numpy
+
 from src.tools.point import Point
 from src.tools.segment import Segment
 from src.environment.surface import Surface
@@ -42,3 +44,14 @@ def scoring_distance(**kargs):
     if distance == 0:
         return 200
     return round(100*(1 - abs(distance)/kargs.get("surface").length))
+
+
+def scoring_speed(**kargs):
+    if kargs.get("on_site"):
+        score = max(0, 100*(1 - abs(kargs.get("lander").v_speed)/200))
+        score += max(0, 80*(1 - abs(kargs.get("lander").h_speed)/200))
+    else:
+        abs_speed = numpy.sqrt(kargs.get("lander").v_speed**2 + kargs.get("lander").h_speed**2)
+        score = max(0, round(20*(1 - abs_speed/150))) # 150 : max speed estimated
+        score = 0
+    return score
